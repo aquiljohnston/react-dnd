dd4890b24530f2fe3c56fbf208578ed4007b8397
aquiljohnston <fulldev@Fullstack-Devs-iMac-Pro.local>
2020-07-10 23:19:03 +1000

    github page

---
+++ precache-manifest.7dbbbffdf053cf5b8e75d3c7d7e85db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237eb8eac548c1e432fd0a5c179ad510",
+    "url": "/react-dnd/index.html"
+  },
+  {
+    "revision": "e2f9277955ccc63f85ff",
+    "url": "/react-dnd/static/css/main.e0092dc2.chunk.css"
+  },
+  {
+    "revision": "955b49593e175fa65ae3",
+    "url": "/react-dnd/static/js/2.385f59e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-dnd/static/js/2.385f59e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f9277955ccc63f85ff",
+    "url": "/react-dnd/static/js/main.2baf6618.chunk.js"
+  },
+  {
+    "revision": "dbd62100ade7d788641c",
+    "url": "/react-dnd/static/js/runtime-main.cf45d206.js"
+  }
+]);
